da54c5af645e15d51ef06cb25156a44e2b4e057d
Lucas Ramos <lucasrcdias@gmail.com>
2016-05-10 11:39:23 -0300

    Implementa rota padrão

--- src/js/routes/home/home.routes.js
+++ src/js/routes/home/home.routes.js
@@ -1,7 +1,10 @@
 (function() {
-  angular.module('imperium').config(['$stateProvider', HomeRoutes]);
+  angular.module('imperium').config(['$stateProvider', '$urlRouterProvider', HomeRoutes]);
 
-  function HomeRoutes($stateProvider) {
+  function HomeRoutes($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
     $stateProvider.state('home', {
       url: '/home',
       templateUrl: "home.html"
