a58a075758ec4e6a116ef52fd495273a7581473b
Lucas Ramos <lucasrcdias@gmail.com>
2016-05-10 11:46:32 -0300

    Organiza código

--- src/js/routes/auth/auth.controller.js
+++ src/js/routes/auth/auth.controller.js
@@ -2,9 +2,9 @@
   angular.module('imperium').controller('AuthCtrl', ['$scope', '$timeout', AuthCtrl]);
 
   function AuthCtrl($scope, $timeout) {
-    $scope.emailRegex =/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+    $scope.user           = {};
+    $scope.emailRegex     = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
     $scope.authenticating = false;
-    $scope.user = {};
 
     $scope.authUser = function (user) {
       $scope.authenticating = true;
