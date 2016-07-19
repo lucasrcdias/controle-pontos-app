bb5d6634cf9b476150a444c285951001e61b3cf2
Lucas Ramos <lucasrcdias@gmail.com>
2016-07-18 22:26:21 -0300

    Implementa listener para alteração no login e simula logout

--- www/js/directives/navbar.directive.js
+++ www/js/directives/navbar.directive.js
@@ -6,7 +6,31 @@
   function imNavbar() {
     var directive = {
       restrict: "E",
-      templateUrl: "navbar.html"
+      templateUrl: "navbar.html",
+      controller: imNavbarCtrl,
+      controllerAs: "vm",
+      bindToController: true
+    };
+
+    imNavbarCtrl.$inject = ["$rootScope", "$state"];
+
+    function imNavbarCtrl($rootScope, $state) {
+      var vm = this;
+
+      vm.isAuthenticated = false;
+
+      vm.signOut = signOut;
+
+      $rootScope.$on("authChanged", updateUser);
+
+      function updateUser(event, isAuthenticated) {
+        vm.isAuthenticated = isAuthenticated;
+      };
+
+      function signOut() {
+        $rootScope.$emit("authChanged", false);
+        $state.go("auth");
+      };
     };
 
     return directive;
